98b93afa6e561dfba0465310801551f682970603
Joel Haag <joelohaag@gmail.com>
2022-02-07 21:24:34 +0200

    debug set to false

--- NexusFinal/settings.py
+++ NexusFinal/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["www.thenexusfest.co.za/"]
+ALLOWED_HOSTS = ["www.thenexusfest.co.za/", "www.nexusfinal.herokuapp.com"]
 
 
 # Application definition
