42aafa0483aea82766d31fbe4f99f87eafafa398
Joel Haag <joelohaag@gmail.com>
2022-02-09 01:53:23 +0200

    changed settings

--- NexusFinal/settings.py
+++ NexusFinal/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
