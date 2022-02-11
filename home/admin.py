8eec31ec4047d83c39b05c44b39ff9fea5d785e9
Joel Haag <joelohaag@gmail.com>
2022-02-11 23:33:46 +0200

    added to adminsection

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import DateRel, Artist
+from .models import DateRel, Artist, AboutInfo, Attraction
 
 # Register your models here.
 
 admin.site.register(Artist)
+admin.site.register(AboutInfo)
+admin.site.register(Attraction)
 admin.site.register(DateRel)
